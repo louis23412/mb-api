9b2d1a3788b5472ef7d395758ea83e1bf9395a46
Louis <louis23412@gmail.com>
2025-04-20 03:07:04 +0200

    implemented viewUserProfile resolver

--- src/graphql-collections/users/users.hadlers.js
+++ src/graphql-collections/users/users.hadlers.js
@@ -1,15 +1,53 @@
 import usersDatabase from '../../models/users.mongo.js';
 
-function calculateLvl(exp) {
-    // calculate level
+function calculateUserLevel(totalExperience) {
+    const baseLvl = 1000;
+    let userLvl;
 
-    // return lvl
+    if (totalExperience < baseLvl) {
+        userLvl = totalExperience / baseLvl
+    } else {
+        userLvl = (Math.log(totalExperience / baseLvl) ** 2.5) + 1;
+    }
+
+    return userLvl.toFixed(2);
 }
 
 export async function viewUserProfile(username) {
-    // find username here
+    try {
+        const answer = await usersDatabase.findOne({
+            username
+        },
+        {
+            hash : 0,
+            defaultPlanet : 0,
+            planetList : 0,
+            totalPlanets : 0,
+            __v : 0,
+            _id : 0,
+            email : 0,
+            lastLogin : 0
+        })
 
-    // check if planet exists for user
+        if (answer.username) {
+            return {
+                status : true,
+                profile : {
+                    username,
+                    lvl : calculateUserLevel(answer.exp),
+                    currencies : answer.currencies
+                }
+            }
+        }
 
-    return false
+        return {
+            status : false,
+            message : 'failed to find user'
+        }
+    } catch (err) {
+        return {
+            status : false,
+            message : 'failed to fetch user'
+        }
+    }
 }
